23f7ef42199c71b49893915e8fbe8332c2845f7d
David Parsons <orc@crepe.pell.portland.or.us>
2010-04-17 11:34:38 -0700

    Somehow the check for nonzero domain state got backed out of domain.c?  Fix that now.

--- domain.c
+++ domain.c
@@ -90,7 +90,7 @@ initdomain()
 		domain = strtok(0, ":");
 		active = strtok(0, ":");
 
-		if (owner && domain && active)
+		if ( owner && domain && active && atoi(active) )
 		    add_dom(domain,VSPOOL,VPATH);
 	    }
 	    fclose(f);
